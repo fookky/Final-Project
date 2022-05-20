d92852695b227a464e237f9d076ff155a7f8c7f7
fookky <32532895+fookky@users.noreply.github.com>
2022-05-20 12:26:15 +0700

    Fookky commit
    Fookky commit

--- src/views/regis.js
+++ src/views/regis.js
@@ -42,8 +42,8 @@ const Register = () => {
       FirstName,
       LastName,
       Email,
-      // Register_Date,
-      // Register_Year_Mounth,
+      Register_Date,
+      Register_Year_Mounth,
       Uid,
       Role
       // FoodList: [],
